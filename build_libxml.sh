2a550645d328e996ef729994b6d33d72d1ee1803
eric11220 <b99902080@ntu.edu.tw>
2018-01-25 10:34:49 +0800

    Add build_libxml.sh

---
+++ build_libxml.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo $0: usage: cross_compile_library.sh ARCH 
+    echo "example: usage: cross_compile_library.sh [ arm-linux | arm-linux-gnueabihf | arm-linux-gnueabi ]"
+    exit 1
+fi
+
+tool_chain_path=$1
+
+# linux architecture 
+item=`ls $tool_chain_path/bin | grep gcc`
+IFS=' ' read -ra ADDR <<< "$item"
+item="${ADDR[0]}"
+ARCH=`echo $item | sed -e 's/-gcc.*//g'`
+
+# ======== libxml with static build ========
+export ARCH=$ARCH
+export AR=${ARCH}-ar
+export AS=${ARCH}-as
+export LD=${ARCH}-ld
+export RANLIB=${ARCH}-ranlib
+export CC=${ARCH}-gcc
+export NM=${ARCH}-nm
+
+./autogen.sh
+./configure --host=${ARCH} --prefix=$tool_chain_path ARCH=${ARCH} --without-python --without-iconv --without-zlib --without-lzma
+make clean
+make 
+sudo "PATH=$PATH" make install
